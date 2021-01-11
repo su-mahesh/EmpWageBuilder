899c845fb46225e394b4bd02ab39a665648a5c37
su-mahesh <maheshkangude@gmail.com>
2021-01-11 20:17:17 +0530

    claculating wages till working hours of 100 or max days of 20

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,35 +1,44 @@
 class EmpWageBuilder
 {
-		public static void main(String[] args)
+	public static void main(String[] args)
 {
 	//constants
 	final	int IS_FULL_TIME = 1;
-	final int IS_PART_TIME=2;
+	final int IS_PART_TIME = 2;
 	final int EMP_RATE_PER_HOUR = 20;
 	final int NUM_WORKING_DAYS = 20;
+	final int MAX_WORKING_HOURS = 100;
+
 	//variable
 	int empHrs = 0;
 	int empWage = 0;
 	int totalEmpWage = 0;
+	int totalEmpWorkingHrs = 0;
+	int totalEmpWorkingDays = 0;
+
 	//computation
-for( int Day = 1; Day <= NUM_WORKING_DAYS; Day++)
+while ( totalEmpWorkingDays <= NUM_WORKING_DAYS && totalEmpWorkingHrs < MAX_WORKING_HOURS )
 {	int empCheck = (int)(Math.random() * 10) % 3;
 
 	switch ( empCheck ){
 		case IS_FULL_TIME:
 			empHrs=8;
+			totalEmpWorkingDays++;
 			break;
 		case IS_PART_TIME:
 			empHrs = 4;
+			totalEmpWorkingDays++;
 			break;
 		default:
 			empHrs=0;
 }
+	totalEmpWorkingHrs += empHrs;
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	totalEmpWage += empWage;
 	System.out.println("Emp wage: " + empWage );
+
 }
-	System.out.println("Total Wage: " + totalEmpWage );
+	System.out.println("Total working days: "+ totalEmpWorkingDays +" Total Working Hours: " + totalEmpWorkingHrs + " Total Wage: " + totalEmpWage );
 }
 
 }
