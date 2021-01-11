f5605d2e2124b6459d48f56fd804cc9ddf8ef857
su-mahesh <maheshkangude@gmail.com>
2021-01-11 19:20:32 +0530

    calculate employee wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,17 +3,19 @@ class EmpWageBuilder
 	public static void main(String[] args)
 {
 	//constants
-	int IS_FULL_TIME = 1;
-
+	final	int IS_FULL_TIME = 1;
+	final int EMP_RATE_PER_HOUR = 20;
+	//variable
+	int empHrs = 0;
+	int empWage = 0;
 	//computation
 	int empCheck = (int)(Math.random() * 10) % 2;
 
 	if ( empCheck == IS_FULL_TIME )
-	System.out.println("Employee is present");
+		empHrs=8;
 
-	else
-	System.out.println("Employee is absent");
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Employee Wage: " +empWage);
 
 }
-
 }
